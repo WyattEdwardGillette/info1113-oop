590b6c6f9847c470e68c07514b6dea059f5010c3
nguyenpk <58803185+WyattEdwardGillette@users.noreply.github.com>
2022-09-01 01:02:52 +1000

    Create TruthInTheMiddle_Solution.java

---
+++ WeeklyTasks/Task0/TruthInTheMiddle_Solution.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class TruthInTheMiddle_Solution{
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        if(n <=1 || n %2 == 0){
+            System.out.println("Invalid size of the matrix");
+            return;
+        }
+
+        int middle = (n+1)/2;
+
+        int countTruth = 0;
+        int columnTruth = 0;
+        int rowTruth = 0;
+
+        for(int column = 1; column <= n; ++column){
+            for(int row = 1; row <= n; ++row){
+                if(scan.next().charAt(0) == 'T'){
+                    ++countTruth;
+                    columnTruth = column;
+                    rowTruth = row;        
+                }
+            }
+        }
+
+        if(countTruth != 1){
+            System.out.println("Error! Exactly one truth please");
+            return;
+        }
+
+        System.out.println(Math.abs(middle - columnTruth) + Math.abs(middle - rowTruth));
+    }
+}
